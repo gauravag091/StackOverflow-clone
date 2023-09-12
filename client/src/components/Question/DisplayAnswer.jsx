d9fa665acfe58aeb017c7caa446a6daca91b0ee7
Gaurav agarwal <53783823+gauravag091@users.noreply.github.com>
2023-09-12 21:08:33 +0530

    Update DisplayAnswer.jsx

--- client/src/components/Question/DisplayAnswer.jsx
+++ client/src/components/Question/DisplayAnswer.jsx
@@ -35,7 +35,7 @@ const DisplayAnswer = ({question,handleShare}) => {
                     <div>
                         <p>{t('answered')} {moment(ans.answeredOn).fromNow()}</p>
                         <Link to={`/Users/${ans.userId}`} className='user-link' style={{color:'#0086d8'}}>
-                          <Avatar backgroundColor='green' px='8px' py='5px'>{ans.userAnswered.charAt(0).toUpperCase()}</Avatar>
+                          <Avatar backgroundColor='green' px='8px' py='5px'>{ans?.userAnswered?.trim()?.charAt(0).toUpperCase()}</Avatar>
                           <div>
                             {ans.userAnswered}
                           </div>
